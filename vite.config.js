0bf210fc5b8192aca9ac6f4a664963438847380e
kasimtp <tpkasimvlr@gmail.com>
2026-06-17 09:42:41 +0530

    nre

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   base: '/club/',
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/club/',
+  plugins: [react()],
+  build: {
+    outDir: 'dist' // Ensure this matches Vercel's "Output Directory" setting
+  }
 })
